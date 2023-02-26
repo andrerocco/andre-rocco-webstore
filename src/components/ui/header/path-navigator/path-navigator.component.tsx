68d012aafae0e6189e1bd48a8d367635c9b0c0d1
Andre Rocco <andre.amaral.rocco@gmail.com>
2023-02-26 10:58:45 -0300

    Criado componente 'PathNavigator'

---
+++ src/components/ui/header/path-navigator/path-navigator.component.tsx
@@ -0,0 +1,35 @@
+import styles from './path-navigator.module.css';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+
+interface PathNavigatorProps {
+    rootLabel: string;
+}
+
+export default function PathNavigator({ rootLabel }: PathNavigatorProps) {
+    const router = useRouter();
+    // Split the path into an array of paths
+    const isRouteReady = router?.isReady;
+    const pathsArray = router?.asPath.split('/').filter((item) => item !== '');
+
+    return (
+        <aside className={styles.aside}>
+            <nav className={styles.nav}>
+                <span>
+                    <Link href="/">{rootLabel}</Link>
+                </span>
+                {isRouteReady &&
+                    pathsArray.map((path, index) => {
+                        // Creates a full path from the root to the current path
+                        const relativePath = pathsArray ? '/' + pathsArray.slice(0, index + 1).join('/') : '/';
+                        return (
+                            <span key={index}>
+                                {' / '}
+                                <Link href={relativePath}>{path}</Link>
+                            </span>
+                        );
+                    })}
+            </nav>
+        </aside>
+    );
+}
