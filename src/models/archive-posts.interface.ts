49aa96a53c2332912141078b348cb1f1c308ca1b
Andre Rocco <andre.amaral.rocco@gmail.com>
2023-03-03 00:14:17 -0300

    Corrigido tipo do Slug dos 'archivePosts'

--- src/models/archive-posts.interface.ts
+++ src/models/archive-posts.interface.ts
@@ -1,9 +1,9 @@
-import { SanityDocument } from '@sanity/types';
+import { SanityDocument, Slug } from '@sanity/types';
 
 export interface IArchivePost extends SanityDocument {
     data: {
         title: string;
-        slug: { current: string };
+        slug: Slug;
         seasson?: string;
         price?: number;
         released: {
