78b470cd436527811723c9d41f4d20c31798f891
André Amaral Rocco <andre.amaral.rocco@gmail.com>
2023-04-05 14:50:22 -0300

    Corrigido erro de tipo

--- src/components/archive-page/post-introduction/post-introduction.component.tsx
+++ src/components/archive-page/post-introduction/post-introduction.component.tsx
@@ -1,29 +1,33 @@
 import styles from './post-introduction.module.css';
 
 interface PostIntroductionProps {
-    title: string;
-    description: string;
+    title?: string;
+    description?: string;
 }
 
 export default function PostIntroduction({ title, description }: PostIntroductionProps) {
     return (
         <div className={styles.container}>
             <div className={styles.text_wrapper}>
-                <h1>
-                    Aviator Cropped Leather Jacket
-                    <br />
-                    SS2023
-                </h1>
-                <p>
-                    A jaqueta de couro é um clássico que nunca sai de moda. Ela é um item essencial no guarda-roupa de
-                    qualquer homem, pois é uma peça que combina com qualquer ocasião. Além disso, ela é super versátil e
-                    pode ser usada em qualquer estação do ano. A jaqueta de couro é um clássico que nunca sai de moda.
-                    Ela é um item essencial no guarda-roupa de qualquer homem, pois é uma peça que combina com qualquer
-                    ocasião. Além disso, ela é super versátil e pode ser usada em qualquer estação do ano. A jaqueta de
-                    couro é um clássico que nunca sai de moda. Ela é um item essencial no guarda-roupa de qualquer
-                    homem, pois é uma peça que combina com qualquer ocasião. Além disso, ela é super versátil e pode ser
-                    usada em qualquer estação do ano.
-                </p>
+                {title && (
+                    <h1>
+                        Aviator Cropped Leather Jacket
+                        <br />
+                        SS2023
+                    </h1>
+                )}
+                {description && (
+                    <p>
+                        A jaqueta de couro é um clássico que nunca sai de moda. Ela é um item essencial no guarda-roupa
+                        de qualquer homem, pois é uma peça que combina com qualquer ocasião. Além disso, ela é super
+                        versátil e pode ser usada em qualquer estação do ano. A jaqueta de couro é um clássico que nunca
+                        sai de moda. Ela é um item essencial no guarda-roupa de qualquer homem, pois é uma peça que
+                        combina com qualquer ocasião. Além disso, ela é super versátil e pode ser usada em qualquer
+                        estação do ano. A jaqueta de couro é um clássico que nunca sai de moda. Ela é um item essencial
+                        no guarda-roupa de qualquer homem, pois é uma peça que combina com qualquer ocasião. Além disso,
+                        ela é super versátil e pode ser usada em qualquer estação do ano.
+                    </p>
+                )}
             </div>
         </div>
     );
