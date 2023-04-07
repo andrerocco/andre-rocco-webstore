f6014f373c960a9eb49db68c89a5289d509e7c63
Andre Rocco <andre.amaral.rocco@gmail.com>
2023-04-07 16:38:28 -0300

    Corrigido erro de estilo

--- src/components/archive-page/image-grid/image-grid-row/image-grid-row.component.tsx
+++ src/components/archive-page/image-grid/image-grid-row/image-grid-row.component.tsx
@@ -18,9 +18,9 @@ export default function ImageGridRow({ layout, imageList }: ImageGridRowProps) {
         if (layoutArray.every((val, index) => val === ['horizontal', 'horizontal'][index])) {
             return styles.grid_hh;
         } else if (layoutArray.every((val, index) => val === ['horizontal', 'vertical'][index])) {
-            return styles.grid_vh;
-        } else if (layoutArray.every((val, index) => val === ['vertical', 'horizontal'][index])) {
             return styles.grid_hv;
+        } else if (layoutArray.every((val, index) => val === ['vertical', 'horizontal'][index])) {
+            return styles.grid_vh;
         } else if (layoutArray.every((val, index) => val === ['vertical', 'vertical', 'vertical'][index])) {
             return styles.grid_vvv;
         }
